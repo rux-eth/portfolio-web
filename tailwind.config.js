60eaf9d366f7dac530cac4fbffd40362cfb28f3c
TheBigMort <thebigmort1249@gmail.com>
2022-06-08 10:33:11 -0500

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
-import { themeConstants as tc } from "./styles/theme";
 const defaultTheme = require("tailwindcss/defaultTheme");
-
-let themeConstants = tc;
-for (const val in themeConstants.breakpoints) {
-  themeConstants.breakpoints[val] = `${val}px`;
-}
+const themeConstants = {
+  paper: "#F9F9F9",
+  primary: {
+    main: "#ffffff",
+    dark: "#2f2f2f",
+  },
+  secondary: {
+    main: "#2f2f2f",
+    dark: "#ffffff",
+  },
+  error: {
+    main: "#b22222",
+    dark: "#8b0000",
+  },
+  fg: { main: "#fff", dark: "rgba(55, 65, 81, 1)" },
+  breakpoints: {
+    xs: "0px",
+    mb: "350px",
+    sm: "600px",
+    md: "960px",
+    lg: "1280px",
+    xl: "1920px",
+  },
+};
 
 module.exports = {
   mode: "jit",
